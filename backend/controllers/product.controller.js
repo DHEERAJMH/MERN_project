cf33ba999cd2d1f1f75fa11a68c4e5bc0e887c19
Dheeraj <xyz@example.com>
2024-12-13 09:18:45 +0530

    modified backend

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -48,10 +48,14 @@ export const updateProduct = async (req,res)=>{
 export const deleteProduct = async (req,res)=>{
     const {id} = req.params;
     
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({success:false,message:"invalid user"});
+    }
+
     try {
         await Product.findByIdAndDelete(id); 
         res.status(200).json({success:true,message:"product deleted successfully"});
     } catch (error) {
-        res.status(404).json({success:false,message:"prdouct not found"});
+        res.status(500).json({success:false,message:"server error"});
     }
 };
